fe419ffa7b3ad22a5d6d28e80a49ff606ff32de5
DEBASHIS DEY <ddey23006@gmail.com>
2024-05-09 19:58:12 +0530

    Added jobController.js and modified jobRoutes.js

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { getAllJobs } from "../controllers/jobController.js";
 
 const router = express.Router();
+
+router.get("/getall", getAllJobs);
 
 export default router;
