0b248c76245f83ad40617efbb39b5ff62febec10
DEBASHIS DEY <ddey23006@gmail.com>
2024-05-12 04:07:45 +0530

    Added getUser funtion

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { register, login, logout } from "../controllers/userController.js";
+import {
+  register,
+  login,
+  logout,
+  getUser,
+} from "../controllers/userController.js";
 import { isAuthorized } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -7,5 +12,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", isAuthorized, logout);
+router.get("/getuser", isAuthorized, getUser);
 
 export default router;
