6c996edc0000d2230c44ad2166787bc481021a44
DEBASHIS DEY <ddey23006@gmail.com>
2024-06-01 13:44:28 +0530

    secure true cookie edit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,7 +5,7 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
     ),
     httpOnly: true, // Set httpOnly to true
-
+    secure: true,
     sameSite: "strict",
   };
 
