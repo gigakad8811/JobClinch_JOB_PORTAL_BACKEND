f5dc3469c5f00eb71eadc6a7c45db744ba585f72
DEBASHIS DEY <ddey23006@gmail.com>
2024-06-01 13:40:01 +0530

    cookie edit

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,7 +66,7 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
       httpOnly: true,
       expires: new Date(Date.now()),
 
-      sameSite: "lax",
+      sameSite: "strict",
     })
     .json({
       success: true,
